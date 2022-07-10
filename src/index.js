414a3af519baf1f84896ea3264fe82d88fdd89db
axense234 <andreicomanescuonline@gmail.com>
2022-07-10 14:10:45 +0300

    Changed title name and changed the way the project works with netlify so now whenever i change something in the project i wont need to run npm run build

--- src/index.js
+++ src/index.js
@@ -1,17 +1,113 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { StrictMode, useEffect, useRef, useState } from "react";
+import { createRoot } from "react-dom/client";
+// Employees
+import { people } from "./people";
+// CSS
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const MainApp = () => {
+  const [tabDetailsID, setTabDetailsID] = useState(1);
+  const [changeMode, setChangeMode] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      if (window.innerWidth <= 800) {
+        setChangeMode(true);
+      } else {
+        setChangeMode(false);
+      }
+    });
+
+    return () => {
+      window.removeEventListener("resize", console.log("removed"));
+    };
+  });
+
+  return (
+    <>
+      <div className='title-container'>
+        <h1>Experience</h1>
+        <hr />
+      </div>
+      {changeMode ? (
+        <section className='main-content active'>
+          <TabsSection
+            employees={people}
+            setID={setTabDetailsID}
+            mode={changeMode}
+            currentID={tabDetailsID}
+          ></TabsSection>
+          <TabsDetails
+            employee={people[tabDetailsID - 1]}
+            tabs={people[tabDetailsID - 1].personTabs}
+          ></TabsDetails>
+        </section>
+      ) : (
+        <section className='main-content'>
+          <TabsSection employees={people} setID={setTabDetailsID}></TabsSection>
+          <TabsDetails
+            employee={people[tabDetailsID - 1]}
+            tabs={people[tabDetailsID - 1].personTabs}
+          ></TabsDetails>
+        </section>
+      )}
+    </>
+  );
+};
+
+const TabsSection = ({ employees, setID, mode, currentID }) => {
+  const [active, setActive] = useState(false);
+  const manageClick = (id) => {
+    console.log(currentID);
+    if (id === currentID) {
+      setActive(true);
+    } else {
+      setActive(false);
+    }
+  };
+
+  return (
+    <>
+      <div className={mode ? "tabs-names oblical" : "tabs-names"}>
+        {employees.map((employee) => {
+          return (
+            <button
+              className='tab-name'
+              onClick={() => manageClick(employee.id)}
+              key={employee.id}
+            >
+              {employee.personName.toUpperCase()}
+            </button>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+const TabsDetails = ({ tabs, employee }) => {
+  return (
+    <>
+      <div className='tabs-details'>
+        <p id='tabs-job'>{employee.personJob}</p>
+        <p id='tabs-name'>{employee.personName.toUpperCase()}</p>
+        <p id='tabs-tenure'>{employee.personTenure}</p>
+        {tabs.map((tab) => {
+          return (
+            <div className='personTab' key={tab.id}>
+              <p>{tab.content}</p>
+            </div>
+          );
+        })}
+        <button id='more-info'>MORE INFO</button>
+      </div>
+    </>
+  );
+};
+
+const root = createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <MainApp></MainApp>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/people.js
@@ -0,0 +1,76 @@
+export const people = [
+  {
+    id: 1,
+    personName: "Tommy",
+    personJob: "Full Stack Web Developer",
+    personTenure: "December 2015 - Present",
+    personTabs: [
+      {
+        id: 1,
+        content:
+          "Tote bag sartorial mlkshk air plant vinyl banjo lumbersexual poke leggings offal cold-pressed brunch neutra. Hammock photo booth live-edge disrupt.",
+      },
+      {
+        id: 2,
+        content:
+          "Post-ironic selvage chambray sartorial freegan meditation. Chambray chartreuse kombucha meditation, man bun four dollar toast street art cloud bread live-edge heirloom.",
+      },
+      {
+        id: 3,
+        content:
+          "Butcher drinking vinegar franzen authentic messenger bag copper mug food truck taxidermy. Mumblecore lomo echo park readymade iPhone migas single-origin coffee franzen cloud bread tilde vegan flexitarian.",
+      },
+    ],
+  },
+  {
+    id: 2,
+    personName: "Bigdrop",
+    personJob: "Front-End Engineer",
+    personTenure: "May 2015 - December 2015",
+    personTabs: [
+      {
+        id: 1,
+        content:
+          "Hashtag drinking vinegar scenester mumblecore snackwave four dollar toast, lumbersexual XOXO. Cardigan church-key pabst, biodiesel vexillologist viral squid.",
+      },
+      {
+        id: 2,
+        content:
+          "Franzen af pitchfork, mumblecore try-hard kogi XOXO roof party la croix cardigan neutra retro tattooed copper mug. Meditation lomo biodiesel scenester.",
+      },
+      {
+        id: 3,
+        content:
+          "Fam VHS enamel pin try-hard echo park raw denim unicorn fanny pack vape authentic. Helvetica fixie church-key, small batch jianbing messenger bag scenester +1",
+      },
+      {
+        id: 4,
+        content:
+          "Fam VHS enamel pin try-hard echo park raw denim unicorn fanny pack vape authentic. Helvetica fixie church-key, small batch jianbing messenger bag scenester +1",
+      },
+    ],
+  },
+  {
+    id: 3,
+    personName: "Cuker",
+    personJob: "Engineering Intern",
+    personTenure: "May 2014 - September 2015",
+    personTabs: [
+      {
+        id: 1,
+        content:
+          "I'm baby woke mumblecore stumptown enamel pin. Snackwave prism pork belly, blog vape four loko sriracha messenger bag jean shorts DIY bushwick VHS. Banjo post-ironic hella af, palo santo craft beer gluten-free.",
+      },
+      {
+        id: 2,
+        content:
+          "YOLO drinking vinegar chambray pok pok selfies quinoa kinfolk pitchfork street art la croix unicorn DIY. Woke offal jianbing venmo tote bag, palo santo subway tile slow-carb post-ironic pug ugh taxidermy squid.",
+      },
+      {
+        id: 3,
+        content:
+          "Pour-over glossier chambray umami 3 wolf moon. Iceland kale chips asymmetrical craft beer actually forage, biodiesel tattooed fingerstache. Pork belly lomo man braid, portland pitchfork locavore man bun prism.",
+      },
+    ],
+  },
+];
